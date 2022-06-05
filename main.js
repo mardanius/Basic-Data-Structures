056b0f4375631626bfb9e3241ad664bcf28fe201
mardanius <mariojeda.mg@gmail.com>
2022-06-05 14:04:19 -0400

    Added 'Splice and Slide methods'

--- main.js
+++ main.js
@@ -133,3 +133,146 @@ function mixedNumbers(arr) {
 
 console.log(mixedNumbers(['IV', 5, 'six']));
 
+
+
+
+/*-----------------------------------------------
+Remove Items from an Array with pop() and shift()
+-------------------------------------------------
+
+Both push() and unshift() have corresponding methods that are nearly functional opposites: pop() and shift(). As you may have guessed by now, 
+instead of adding, pop() removes an element from the end of an array, while shift() removes an element from the beginning. The key difference 
+between pop() and shift() and their cousins push() and unshift(), is that neither method takes parameters, and each only allows an array to 
+be modified by a single element at a time.
+
+Let's take a look:
+
+let greetings = ['whats up?', 'hello', 'see ya!'];
+
+greetings.pop();
+greetings would have the value ['whats up?', 'hello'].
+
+greetings.shift();
+greetings would have the value ['hello'].
+
+We can also return the value of the removed element with either method like this:
+
+let popped = greetings.pop();
+greetings would have the value [], and popped would have the value hello.
+
+-----------------------------------------------------------------------------------------------------------------------------------------------
+We have defined a function, popShift, which takes an array as an argument and returns a new array. Modify the function, using pop() and shift(), 
+to remove the first and last elements of the argument array, and assign the removed elements to their corresponding variables, so that the
+returned array contains their values.
+*/
+
+function popShift(arr) {
+  let popped = arr.pop(); // Change this line
+  let shifted = arr.shift(); // Change this line
+  return [shifted, popped];
+}
+
+console.log(popShift(['challenge', 'is', 'not', 'complete']));
+
+
+
+
+/*-------------------------
+Remove Items Using splice()
+---------------------------
+
+Ok, so we've learned how to remove elements from the beginning and end of arrays using shift() and pop(), but what if we want to remove an 
+element from somewhere in the middle? Or remove more than one element at once? Well, that's where splice() comes in. splice() allows us to 
+do just that: remove any number of consecutive elements from anywhere in an array.
+
+splice() can take up to 3 parameters, but for now, we'll focus on just the first 2. The first two parameters of splice() are integers which 
+represent indexes, or positions, of items in the array that splice() is being called upon. And remember, arrays are zero-indexed, so to indicate 
+the first element of an array, we would use 0. splice()'s first parameter represents the index on the array from which to begin removing elements, 
+while the second parameter indicates the number of elements to delete. For example:
+
+let array = ['today', 'was', 'not', 'so', 'great'];
+
+array.splice(2, 2);
+Here we remove 2 elements, beginning with the third element (at index 2). array would have the value ['today', 'was', 'great'].
+
+splice() not only modifies the array it's being called on, but it also returns a new array containing the value of the removed elements:
+
+let array = ['I', 'am', 'feeling', 'really', 'happy'];
+
+let newArray = array.splice(3, 2);
+newArray has the value ['really', 'happy'].
+
+------------------------------------------------------------------------------------------------------------------------------------------
+We've initialized an array arr. Use splice() to remove elements from arr, so that it only contains elements that sum to the value of 10.*/
+
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// Only change code below this line
+arr.splice(1, 4);
+// Only change code above this line
+console.log(arr);
+
+
+
+
+/*----------------------
+Add Items Using splice()
+------------------------
+
+Remember in the last challenge we mentioned that splice() can take up to three parameters? Well, you can use the third parameter, comprised 
+of one or more element(s), to add to the array. This can be incredibly useful for quickly switching out an element, or a set of elements, for another.
+
+const numbers = [10, 11, 12, 12, 15];
+const startIndex = 3;
+const amountToDelete = 1;
+
+numbers.splice(startIndex, amountToDelete, 13, 14);
+console.log(numbers);
+The second occurrence of 12 is removed, and we add 13 and 14 at the same index. The numbers array would now be [ 10, 11, 12, 13, 14, 15 ].
+
+Here, we begin with an array of numbers. Then, we pass the following to splice(): The index at which to begin deleting elements (3), the number 
+of elements to be deleted (1), and the remaining arguments (13, 14) will be inserted starting at that same index. Note that there can be any 
+number of elements (separated by commas) following amountToDelete, each of which gets inserted.
+
+--------------------------------------------------------------------------------------------------------------------------------------------
+We have defined a function, htmlColorNames, which takes an array of HTML colors as an argument. Modify the function using splice() to remove 
+the first two elements of the array and add 'DarkSalmon' and 'BlanchedAlmond' in their respective places.*/
+
+function htmlColorNames(arr) {
+  // Only change code below this line
+  arr.splice(0, 2, 'DarkSalmon', 'BlanchedAlmond')
+  // Only change code above this line
+  return arr;
+}
+
+console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
+
+
+
+
+/*----------------------------
+Copy Array Items Using slice()
+------------------------------
+
+The next method we will cover is slice(). Rather than modifying an array, slice() copies or extracts a given number of elements to a new array, 
+leaving the array it is called upon untouched. slice() takes only 2 parameters — the first is the index at which to begin extraction, and the 
+second is the index at which to stop extraction (extraction will occur up to, but not including the element at this index). Consider this:
+
+let weatherConditions = ['rain', 'snow', 'sleet', 'hail', 'clear'];
+
+let todaysWeather = weatherConditions.slice(1, 3);
+todaysWeather would have the value ['snow', 'sleet'], while weatherConditions would still have ['rain', 'snow', 'sleet', 'hail', 'clear'].
+
+In effect, we have created a new array by extracting elements from an existing array.
+
+We have defined a function, forecast, that takes an array as an argument. Modify the function using slice() to extract information from the 
+argument array and return a new array that contains the string elements warm and sunny. */
+
+function forecast(arr) {
+  // Only change code below this line
+  var newArr = arr.slice(2, 4);
+  return newArr;
+}
+
+// Only change code above this line
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+
